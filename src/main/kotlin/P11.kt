00bd34adb5bf64140cfa2c20e7c71d3093941e38
david <mintsandwitch@gmail.com>
2022-03-13 11:40:27 +0900

    studying with LeetCode

---
+++ src/main/kotlin/P11.kt
@@ -0,0 +1,32 @@
+class P11 {
+    // failed to solve
+    // referenced from discussion
+    // https://leetcode.com/problems/container-with-most-water/discuss/200246/Proof-by-formula
+    companion object {
+        fun maxArea(height: IntArray): Int {
+            // 가장 큰 넓이 값을 담을 변수. 이를 반환.
+            var volume = 0
+            // 배열에 저장된 높이 값을 가리키는 왼쪽 포인터
+            var i = 0
+            // 배열에 저장된 높이 값을 가리키는 오른쪽 포인터
+            var j = height.size - 1
+
+            while (i < j) {
+                // 부피 = (i - j) * (height[i], height[j] 중 최소값)
+                // 여기서 (i - j)는 가로변의 길이가 됨
+                volume = volume.coerceAtLeast((j - i) * height[i].coerceAtMost(height[j]))
+                // height[i] < height[j] 일 경우, i를 오른쪽으로 움직이는 경우만 생각함
+                // height[i] > height[j] 일 경우, j를 왼쪽으로 움직이는 경우만 생각함
+                if (height[i] < height[j]) i++
+                else j--
+                // height[i] < height[j] 일 경우 j를 왼쪽으로 움직여서 고려할 때의 부피 값은 그 이전 부피 값보다 무조건 작기 때문
+                // 이 때의 부피 V = (i - j) * height[i]이고 (i, j - 1)일 때를 생각해보자
+                // height[i] < height[j - 1] 일 경우 V' = (i - j - 1) * height[i] 이므로 V > V'
+                // height[i] > height[j - 1] 일 경우 V' = (i - j - 1) * height[j - 1] 이므로 V > V'
+                // 이는 height[i] > height[j] 일 경우 i를 오른쪽으로 움직일 때 부피 값이 그 이전 부피 값보다 항상 작은 것과 동일
+            }
+
+            return volume
+        }
+    }
+}
---
+++ src/main/kotlin/P3.kt
@@ -0,0 +1,36 @@
+class P3 {
+    // failed to solve
+    // referenced from discussion
+    companion object {
+        fun lengthOfLongestSubstring(s: String): Int {
+            // 만약 문자열이 비어있다면 부분 문자열 자체가 없기 때문에 그 즉시 0을 반환하고 종료
+            if (s.isEmpty()) return 0
+
+            // 문자열 안의 문자를 key, 그들의 위치 값을 value로 두는 hash map 유지
+            val map = HashMap<Char, Int>()
+
+            // max는 가장 긴 부분 문자열의 길이 값을 유지
+            var max = 0
+            // j는 가장 긴 부분 문자열이 시작되는 위치 값을 유지
+            var j = 0
+
+            // i는 부분 문자열을 만들기 위해 오른쪽으로 이동하는 포인터 역할
+            // 가장 긴 부분 문자열의 끝 값을 의미하기도 함
+            for (i in s.indices) {
+                // 만약 문자열 i 위치의 문자가 이미 map 안에 있을 경우 == 검사한 부분 문자열 안에 중복되는 문자가 있을 경우
+                if (map.containsKey(s[i])) {
+                    // 중복된 문자가 있던 위치보다 하나 더 오른쪽의 위치 값과 현재 j 값을 비교해, 더 큰 값을 j로 유지
+                    j = j.coerceAtLeast(map[s[i]]!!.plus(1))
+                }
+                // 그 문자를 map 안에 유지하고,
+                map[s[i]] = i
+                // 이 때 i, j의 위치로 만들어지는 부분 문자열의 길이와 max를 비교해, 더 큰 값을 max로 유지
+                max = max.coerceAtLeast(i - j + 1)
+            }
+            // 주의) i와 j는 부분 문자열의 시작과 끝의 위치를 가리키는 포인터 역할이고, 오른쪽으로만 이동
+
+            // 얻은 max 값을 반환.
+            return max
+        }
+    }
+}
