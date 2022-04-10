be63f9c297491875ff3ffa1b48d28166be3180d3
david <mintsandwitch@gmail.com>
2022-04-10 20:41:53 +0900

    studying with LeetCode

---
+++ src/main/kotlin/P1202.kt
@@ -0,0 +1,59 @@
+class P1202 {
+    companion object {
+        // failed to solve
+        // referenced from discussion
+        // https://leetcode.com/problems/smallest-string-with-swaps/solution/
+        // 접근법
+        // 각 index들 : vertex, 각 pair들 : edge로 취급해 graph를 그린다.
+        // 그렇게 그려진 graph를 이용해 주어진 String 안의 글자들을 오름차순해준다.
+
+        private const val N = 100001
+        var visited = BooleanArray(N)
+        private val adj: Array<ArrayList<Int>?> = arrayOfNulls(N)
+
+        fun DFS(string: String, vertex: Int, characters: MutableList<Char>, indices: MutableList<Int>) {
+            characters.add(string[vertex])
+            indices.add(vertex)
+
+            visited[vertex] = true
+
+            for (adjacent in adj[vertex]!!) {
+                if (!visited[adjacent]) {
+                    DFS(string, adjacent, characters, indices)
+                }
+            }
+        }
+
+        fun smallestStringWithSwaps(s: String, pairs: List<List<Int>>): String {
+            for (i in s.indices) {
+                adj[i] = ArrayList()
+            }
+
+            for (edge in pairs) {
+                val source = edge[0]
+                val destination = edge[1]
+
+                adj[source]?.add(destination)
+                adj[destination]?.add(source)
+            }
+
+            val answer = arrayOfNulls<Char>(s.length)
+            for (vertex in s.indices) {
+                if (!visited[vertex]) {
+                    val characters = ArrayList<Char>()
+                    val indices = ArrayList<Int>()
+
+                    DFS(s, vertex, characters, indices)
+
+                    characters.sort()
+                    indices.sort()
+
+                    for (index in characters.indices) {
+                        answer[indices[index]] = characters[index]
+                    }
+                }
+            }
+            return answer.joinToString("")
+        }
+    }
+}
---
+++ src/main/kotlin/P787.kt
@@ -0,0 +1,59 @@
+import java.util.*
+
+
+class P787 {
+    companion object {
+        // failed to solve
+        // referenced from discussion
+        // https://leetcode.com/problems/cheapest-flights-within-k-stops/discuss/686774/SUGGESTION-FOR-BEGINNERS-BFS-or-DIJKSHTRA-or-DP
+        // 접근법
+        // 다익스트라 알고리즘 활용
+        fun findCheapestPrice(n: Int, flights: Array<IntArray>, src: Int, dst: Int, k: Int): Int {
+            val adjMatrix = Array(n) { IntArray(n) }
+
+            for (flight in flights) {
+                adjMatrix[flight[0]][flight[1]] = flight[2]
+            }
+
+            val distances = IntArray(n)
+            val currentStops = IntArray(n)
+
+            Arrays.fill(distances, Integer.MAX_VALUE)
+            Arrays.fill(currentStops, Integer.MAX_VALUE)
+
+            distances[src] = 0
+            currentStops[src] = 0
+
+            val minHeap = PriorityQueue { a: IntArray, b: IntArray ->
+                a[1] - b[1]
+            }
+            minHeap.offer(intArrayOf(src, 0, 0))
+
+            while (minHeap.isNotEmpty()) {
+                val info = minHeap.poll()
+                val node = info[0]
+                val stops = info[2]
+                val cost = info[1]
+
+                if (node == dst) return cost
+
+                if (stops == k + 1) continue
+
+                for (nei in 0 until n) {
+                    if (adjMatrix[node][nei] > 0) {
+                        val dV = distances[nei]
+                        val wUV = adjMatrix[node][nei]
+
+                        if (cost + wUV < dV || stops < currentStops[nei]) {
+                            minHeap.offer(intArrayOf(nei, cost + wUV, stops + 1))
+                            distances[nei] = cost + wUV
+                            currentStops[nei] = stops
+                        }
+                    }
+                }
+            }
+
+            return if (distances[dst] == Integer.MAX_VALUE) -1 else distances[dst]
+        }
+    }
+}
