deb1953fd30f6ac45c8397c973f662651703dae3
david <mintsandwitch@gmail.com>
2022-10-02 15:33:30 +0900

    studying with LeetCode

--- src/main/kotlin/P45.kt
+++ src/main/kotlin/P45.kt
@@ -1,6 +1,8 @@
 import kotlin.math.max
 
 class P45 {
+    // referred from here
+    // https://leetcode.com/problems/jump-game-ii/discuss/18014/Concise-O(n)-one-loop-JAVA-solution-based-on-Greedy
     fun jump(nums: IntArray): Int {
         var jumps = 0
         var curEnd = 0
